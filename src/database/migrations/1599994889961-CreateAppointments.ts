cb7b1c74853675b2689e66c84bdd81e7f4fe7c8c
Hamilton Massango <hamiltonsilva551@gmail.com>
2020-09-19 14:21:25 +0200

    delete eslint 2020

--- src/database/migrations/1599994889961-CreateAppointments.ts
+++ src/database/migrations/1599994889961-CreateAppointments.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateAppointments1599994889961 implements MigrationInterface {
+export default class CreateAppointments1599994889961 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {}
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
